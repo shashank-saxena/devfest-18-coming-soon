4120640b441ac6002f352dbb2d6ef0a1669fb023
Shashank Saxena <shashank@newput.com>
2018-09-21 17:30:42 +0530

    Initial version of page

---
+++ js/counter.js
@@ -0,0 +1,31 @@
+var countDownDate = new Date("Sept 21, 2018 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = now - countDownDate;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in an element with id="demo"
+  // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s ";
+  document.getElementById("counterDays").innerHTML = days
+  document.getElementById("counterHours").innerHTML = hours
+  document.getElementById("counterMins").innerHTML = minutes
+  document.getElementById("counterSecs").innerHTML = seconds
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
